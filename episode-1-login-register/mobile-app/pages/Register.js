04b841a5152901e25e1046ec166d01cdd71a1c93
Jean Rauwers <jrauwers@maystreet.com>
2021-02-11 20:55:05 +0000

    Add - episode 1 first commit

---
+++ episode-1-login-register/mobile-app/pages/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Register = () => {
+  return (
+    <View>
+      <Text>Register PAGE</Text>
+    </View>
+  )
+}
+
+export default Register;
