6c090436d21cae2e9a776b803bb75daafc02acdc
Jean Rauwers <jrauwers@maystreet.com>
2021-02-15 15:57:30 +0000

    Add - episode 3  first commit

---
+++ episode-3-add-create-event-page/mobile-app/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard'
+
+export default function App() {
+  const Stack = createStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnContainer: {
+    position: 'absolute',
+    bottom: 50
+  },
+  text: {
+    color: 'black'
+  }
+});
