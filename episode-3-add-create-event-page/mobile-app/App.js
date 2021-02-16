cabaa9970f167b2e7e736c21a81bdc309ef1777e
Jean Rauwers <jrauwers@maystreet.com>
2021-02-16 13:21:17 +0000

    add App status

--- episode-3-add-create-event-page/mobile-app/App.js
+++ episode-3-add-create-event-page/mobile-app/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
